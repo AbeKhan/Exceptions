7e584376104c9f66a0088fea9eb74457094510c2
AbeKhan <128449118+AbeKhan@users.noreply.github.com>
2023-09-19 15:32:26 -0500

    finished

--- src/App.java
+++ src/App.java
@@ -5,12 +5,39 @@ public class App
     public static void main(String[] args) throws Exception 
     {
         Scanner scan = new Scanner(System.in);
+        boolean exit = false;
+        
         System.out.println("Please enter a number:");
-
-        int num = scan.nextInt();
+        
+      
+       while (exit == false)
+       {
+        try
+        {
+        String holder = scan.nextLine();
+        int num = Integer.parseInt(holder);
+        factor(num);
 
         System.out.println("Value you entered: " + num);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Bad input, please try again.");
+        }
+       }
+      
 
 
+    }
+
+    public static void factor(int holder)
+    {
+        for (int i = 1; i <= holder; i++)
+        {
+            if (holder % i == 0) 
+            {
+                System.out.println( i + " is a factor of " + holder);
+            }
+        }
     }
 }
